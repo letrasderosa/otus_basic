ba0af977f928d588cd024bbbca578a56edc59ad8
creamberry <creamberry@ya.ru>
2024-08-26 19:06:55 +0300

    small code improvements

--- tests/test_example.py
+++ tests/test_example.py
@@ -7,7 +7,7 @@ link2 = "http://selenium1py.pythonanywhere.com/"
 
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.parametrize(
     'test_word', ['Google', 'Yandex']
 )
@@ -31,6 +31,16 @@ def test_google_button(browser):
 
 
 # @pytest.mark.skip
+def test_google_button_transfer(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR,".FPdoLc.lJ9FBc input.RNmpXc").click()
+    assert browser.current_url == "https://doodles.google/", 'URL changed to expected url'
+    time.sleep(5)
+
+    # assert btn.get_attribute("value") == "I'm Feeling Lucky"
+
+
+# @pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     browser.get(link2)
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
@@ -38,7 +48,7 @@ def test_guest_can_go_to_login_page(browser):
     time.sleep(4)
 
 
-# 1:23:45
+# tests:23:45
 
 # pytest -v --browser=firefox  tests\test_example.py
 # pytest -v  --language=en --browser=firefox  tests\test_example.py
